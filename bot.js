d9afef64db17d6d308bd5492aefe3ce50b9f7f4e
IxRawan <46265613+IxRawan@users.noreply.github.com>
2019-01-16 19:09:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
  
 client.on('ready', () => {
+  client.user.setGame('E!help','https://www.twitch.tv/IDK');
   console.log(`Logged in as ${client.user.tag}!`);
   console.log('')
   console.log('')
